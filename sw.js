f7fe4f49824440eeb299714b536eed75d81874de
Kirill <kirill.kokorin@gmail.com>
2026-05-14 13:30:22 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // CarLog Service Worker v2
-const CACHE = "carlog-v3";
+const CACHE = "carlog-v5";
 const SHELL = ["./", "./index.html", "./manifest.json"];
 
 // Установка: кешируем оболочку приложения
